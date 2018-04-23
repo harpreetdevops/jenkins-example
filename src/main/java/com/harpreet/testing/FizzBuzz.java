7479108bdd686b99031cb84911512d99c575612b
harpreetdevops <36718656+harpreetdevops@users.noreply.github.com>
2018-04-23 14:03:16 -0700

    Update FizzBuzz.java

--- src/main/java/com/harpreet/testing/FizzBuzz.java
+++ src/main/java/com/harpreet/testing/FizzBuzz.java
@@ -1,12 +1,4 @@
-<<<<<<< HEAD:src/main/java/com/techprimers/testing/FizzBuzz.java
-<<<<<<< HEAD
 package com.harpeet.testing;
-=======
-package com.harpreet.testing;
->>>>>>> 35eb42e5bd29a66e211a8190bdf7739bbff7a46f
-=======
-package com.harpreet.testing;
->>>>>>> dev:src/main/java/com/harpreet/testing/FizzBuzz.java
 
 public class FizzBuzz {
 
